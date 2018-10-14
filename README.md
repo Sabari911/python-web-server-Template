3f957beb0a6c6c444754372978e5a990ad1af7c1
Michael <michael@olimpo.com>
2018-10-14 13:03:01 +0800

    Added singlethreaded version and updated ReadME

--- README.md
+++ README.md
@@ -7,3 +7,30 @@ ORIGINAL AUTHOR:
 Very simple HTTP server in python that is multi threaded. Default port is 8001. If you want port 80, you need super user access.
 I use this template very often and decided to add multithreading capability.
 This will only execute on python 2.7, if you want to adapt for python 3, you'll need to modify the `print` syntaxes only(i think, not tested)
+
+
+##
+There are 2 files in this project. webserver.py(multithreaded python webserver with lock) and webserver-singlethread.py.
+
+I included both implementations in this project to illustrate differences between multithreading,single threading and using Locks.
+
+## Testing
+How to test singlthreading vs multithreading without Locks
+
+    1.Multithreading test with Lock
+        1. Execute ./webserver.py
+        1. In terminal, type "time curl http://localhost:8001 & time curl http://localhost:8001;" without the quotation marks
+            - This will execute 2 curls simultaneously
+        1. First curl request will respond after 15s. Second curl request will respond after 30s, while second curl is waiting for the lock to be freed, you will see Lock still in use 127.0.0.1 is waiting.. in your python console
+
+    1.Multithreading test without Lock
+        1. Execute ./webserver.py -l
+        1. In terminal, type "time curl http://localhost:8001 & time curl http://localhost:8001;" without the quotation marks
+            - This will execute 2 curls simultaneously
+        1. Both curl requests will end in 15s
+
+    1.Singlethreading
+        1. Execute ./webserver-singlethreaded.py
+        1. In terminal, type "time curl http://localhost:8001 & time curl http://localhost:8001;" without the quotation marks
+            - This will execute 2 curls simultaneously
+        1. First curl request will respond after 15s. Second curl request will respond after 30s
--- webserver-singlethread.py
+++ webserver-singlethread.py
@@ -4,29 +4,15 @@ ORIGINAL AUTHOR:
     https://gist.github.com/bradmontgomery/2219997
 
 Very simple HTTP server in python that is multi threaded. Default port is 8001. If you want port 80, you need super user access.
-I use this template very often and decided to add multithreading capability as well a lock.
 
 This will only execute on python 2.7, if you want to adapt for python 3, you'll need to modify the `print` syntaxes only(i think, not tested)
 
 This file, I've put sleep(15) right after `self.wfile.write("<html><body><h1>hi!</h1></body></html>")`. This is to 
-simulate a kind of long process so that you can see how locking works. Note that in acquire(0), the argument '0' means non blocking. If 
-no argument is passed, the default would be a blocking lock.
+simulate a kind of long process.
 
-Lastly, all examples that can be done with this template are only implemented during HTTP GET requests.
 
 Usage::
-    To get help:
-
-    ./webserver.py -h 
-    set Port number(default: 8001) and set thread Locking mechanism(defaut: Enabled)
-    optional arguments:
-    -h, --help            show this help message and exit
-    -p PORT, --port PORT  specify port number
-    -l, --disable-lockng
-
-    example:
-    python ./webserver.py -p 8002 -l
-    This executes the webserver to listen to port 8002 and disable locking
+    python ./webserver.py -p <port number>
 
 Send a GET request::
     curl http://localhost:8001
@@ -42,12 +28,7 @@ Send a POST request::
 """
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import SocketServer
-from SocketServer import ThreadingMixIn
-from threading import Lock
 from time import sleep #to simulate long operation functions
-
-global_locker = Lock()
-is_locking_mechanism_enabled = True
 
 class S(BaseHTTPRequestHandler):
     def _set_headers(self):
@@ -57,16 +38,9 @@ class S(BaseHTTPRequestHandler):
 
     def do_GET(self):
         self._set_headers()
-
-        while(global_locker.acquire(0) == False and is_locking_mechanism_enabled):
-            sleep(1) #i added a sleep 1 second here so it doesnt get too spammy
-            print "Lock still in use", self.client_address[0], "is waiting.." 
-
         self.wfile.write("<html><body><h1>hi!</h1></body></html>")
         sleep(15) #pretend there's a 15 second long operation on going
 
-        if is_locking_mechanism_enabled:
-            global_locker.release()
 
     def do_HEAD(self):
         self._set_headers()
@@ -76,12 +50,8 @@ class S(BaseHTTPRequestHandler):
         self._set_headers()
         self.wfile.write("<html><body><h1>POST!</h1></body></html>")
 
-class threadedServer(ThreadingMixIn,HTTPServer):
-    '''
-    Handle server in another thread
-    '''    
 
-def run(server_class=threadedServer, handler_class=S, port=8001):
+def run(server_class=HTTPServer, handler_class=S, port=8001):
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
     print 'Starting httpd... at port %s' % (str(port))
@@ -89,12 +59,11 @@ def run(server_class=threadedServer, handler_class=S, port=8001):
     httpd.serve_forever()
 
 if __name__ == "__main__":
+    from sys import argv
     import argparse
     parser = argparse.ArgumentParser(description='set Port number(default: 8001) and set thread Locking mechanism(defaut: Enabled)')
     parser.add_argument('-p','--port', dest='port', action='store', type=int, default=8001,
                         help='specify port number')
-    parser.add_argument('-l','--disable-lockng', dest='is_locking_mechanism_enabled',action='store_false', default=True)
     args = parser.parse_args()    
-    is_locking_mechanism_enabled=args.is_locking_mechanism_enabled
-    print "Locking mechanism is ", is_locking_mechanism_enabled
+
     run(port=args.port)
